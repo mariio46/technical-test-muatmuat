a132165bda2e9afa1109a32cf12bee8520ca086b
Mario <mariomad2296@gmail.com>
2025-02-15 14:56:59 +0800

    Update navigation between pages

--- src/app/(general)/(home)/page.tsx
+++ src/app/(general)/(home)/page.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+
+import { Container } from '@/components/container';
+import { HeaderSubTitle, HeaderTitle } from '@/components/header';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <section id='header' className='border-y'>
+            <Container className='space-y-3 py-24'>
+                <HeaderTitle>Technical Test by Muatmuat</HeaderTitle>
+                <HeaderSubTitle>Welcome to Technical Test by Muatmuat.</HeaderSubTitle>
+                <div className='flex items-center gap-5'>
+                    <Button asChild>
+                        <Link href='/products'>Products</Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href='/pokemons'>Pokemons</Link>
+                    </Button>
+                </div>
+            </Container>
+        </section>
     );
 }
--- src/app/(general)/products/page.tsx
+++ src/app/(general)/products/page.tsx
@@ -2,6 +2,7 @@ import { Container } from '@/components/container';
 import { HeaderSubTitle, HeaderTitle } from '@/components/header';
 
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { ProductContent } from './_content';
 import { ProductCreate } from './_create';
 
@@ -15,9 +16,14 @@ export default function Products() {
                         Your will be never go back to the old way of doing things. Commerce is the future of online
                         shopping.
                     </HeaderSubTitle>
-                    <ProductCreate>
-                        <Button type='button'>Add new product</Button>
-                    </ProductCreate>
+                    <div className='flex items-center gap-5'>
+                        <Button asChild>
+                            <Link href='/'>Back to home</Link>
+                        </Button>
+                        <ProductCreate>
+                            <Button type='button'>Add new product</Button>
+                        </ProductCreate>
+                    </div>
                 </Container>
             </section>
             <section id='content' className='bg-background'>
