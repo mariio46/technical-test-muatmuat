d71bcacefabcfdf025cee4b27ad6f9319314b19e
Mario <mariomad2296@gmail.com>
2025-02-15 12:01:15 +0800

    Update layout.tsx

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,7 +1,3 @@
 export default function GeneralLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <main className='bg-background-secondary min-h-dvh'>
-            <div className='mx-auto max-w-7xl px-4 lg:px-0'>{children}</div>
-        </main>
-    );
+    return <main className='bg-background-secondary min-h-dvh'>{children}</main>;
 }
