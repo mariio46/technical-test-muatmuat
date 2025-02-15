9cdaea4bf7ae55fc25b78cc78efe1e1841b989d1
Mario <mariomad2296@gmail.com>
2025-02-15 10:42:15 +0800

    Initialize new project

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        // Client
+        // readonly NEXT_PUBLIC_BASE_URL: string;
+        // ------------------------------------------
+        // Server
+        // readonly BASE_URL: string;
+    }
+}
