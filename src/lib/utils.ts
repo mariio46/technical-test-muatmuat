2a6851e6a43fe72c46dc87b7b1689a61d5412830
Mario <mariomad2296@gmail.com>
2025-02-15 12:00:53 +0800

    Add utility `toRupiah`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { formatCurrency } from 'usemods';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const toRupiah = (value: string | number): string => {
+    if (typeof value === 'string') {
+        const initialStringValue = value.trim() === '' ? '0' : value.trim();
+
+        const stringResult = formatCurrency(Number(initialStringValue), { decimals: 2, locale: 'id-ID' });
+
+        return stringResult;
+    }
+
+    const numberResult = formatCurrency(value, { decimals: 2, locale: 'id-ID' });
+
+    return numberResult;
+};
