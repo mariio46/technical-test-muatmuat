e00beecf058c960adf0e28252c43930b51bff065
Mario <mariomad2296@gmail.com>
2025-02-15 12:00:15 +0800

    Create container.tsx

---
+++ src/components/container.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Container = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+    ({ className, ...props }, ref) => {
+        return <div className={cn('mx-auto max-w-6xl px-4 xl:px-0', className)} ref={ref} {...props} />;
+    },
+);
+
+export { Container };
